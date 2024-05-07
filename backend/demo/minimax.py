0f88b9cd5cd0ec871b3d9950f390eb744abc73d1
darklord1611 <tavietanh16112004@gmail.com>
2024-05-07 20:15:30 +0700

    reformat code, add rainbow dqn examples

---
+++ backend/demo/minimax.py
@@ -0,0 +1,9 @@
+
+
+class Minimax():
+    def __init(self, board_size):
+        self.board_size = board_size
+    
+    # return most efficient move with specific board state
+    def get_move(board):
+        ...
